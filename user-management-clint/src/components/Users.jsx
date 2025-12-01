b5e671b6089c2018cc0d32542ad7d5a17b5157d0
mjh-shikder <mjhsikder@gmail.com>
2025-12-01 22:25:03 +0600

    post method send clint data to server

--- user-management-clint/src/components/Users.jsx
+++ user-management-clint/src/components/Users.jsx
@@ -1,21 +1,65 @@
-import React, { use } from 'react';
-
+import React, { use } from "react";
 
 const Users = ({ userPromise }) => {
-    
-    const users = use(userPromise);
-    console.log(users);
-    
+  const users = use(userPromise);
+  console.log(users);
 
-    return (
-        <div>
-            <div>
-                {
-                    users.map(user => <p key={user.id}>{user.name} Email: {user.email}</p>)
-                }
-            </div>
-        </div>
-    );
+  const handleAddUser = (e) => {
+    e.preventDefault();
+    const name = e.target.name.value;
+    const email = e.target.email.value;
+    console.log(name, email);
+
+      //   form reset
+      e.target.reset();
+    const newUser = { name, email };
+
+    // send data to the server
+    fetch("http://localhost:4000/user", {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(newUser),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log("after post ", data);
+      });
+  };
+
+  return (
+    <div>
+      <div>
+        <h3>Add a user</h3>
+        <form onSubmit={handleAddUser}>
+          <input name="name" type="text" placeholder="name" />
+          <br />
+          <input name="email" type="email" placeholder="email" />
+          <br />
+          <button>Add User</button>
+        </form>
+      </div>
+      <div>
+        {users.map((user) => (
+          <p key={user.id}>
+            {user.name} Email: {user.email}
+          </p>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Users;
+
+/**
+ *  have to request object send
+ * 1. mention method: post
+ * 2. mention header: about json data in the property of th econtent-type : applicaton/json
+ * 3. body: JSON.stringfy(newUser)
+ *
+ * ------------on the server site
+ * 1. app.use(express.json())
+ *
+ */
--- user-management-server/index.js
+++ user-management-server/index.js
@@ -9,20 +9,29 @@ app.get('/', (req, res) => {
 
 
 app.use(cors());
+app.use(express.json());
 
 const users = [
-    {id: 1, name: 'jubair', email: 'mjhshikder.dhaka@gmail.com'},
-    {id: 2, name: 'antora', email: 'antorahossain@gmail.com'},
-    {id: 3, name: 'sona', email: 'sonababu@gmail.com'},
-    {id: 4, name: 'hossain', email: 'mjhshikder.dhaka@gmail.com'},
-    {id: 5, name: 'shikder', email: 'mjhshikder.dhaka@gmail.com'},
+    { id: 1, name: 'jubair', email: 'mjhshikder.dhaka@gmail.com' },
+    { id: 2, name: 'antora', email: 'antorahossain@gmail.com' },
+    { id: 3, name: 'sona', email: 'sonababu@gmail.com' },
+    { id: 4, name: 'hossain', email: 'mjhshikder.dhaka@gmail.com' },
+    { id: 5, name: 'shikder', email: 'mjhshikder.dhaka@gmail.com' },
 ]
 
 app.get('/user', (req, res) => {
     res.send(users)
 })
 
+app.post('/user', (req, res) => {
+    console.log('post method called', req.body);
+    const newUser = req.body;
+    newUser.id = users.length + 1;
+    users.push(newUser)
+    res.send(newUser)
+})
+
 app.listen(port, () => {
     console.log(`Users Server Started on port:${port}`);
-    
+
 })
