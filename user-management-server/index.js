7f37c24cf674e1dc9c0552687978a61124be83f8
mjh-shikder <mjhsikder@gmail.com>
2025-12-01 16:03:29 +0600

    react clint site added

---
+++ user-management-server/index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 4000;
+
+app.get('/', (req, res) => {
+    res.send('user server is available');
+})
+
+
+const users = [
+    {id: 1, name: 'jubair', email: 'mjhshikder.dhaka@gmail.com'},
+    {id: 2, name: 'antora', email: 'antorahossain@gmail.com'},
+    {id: 3, name: 'sona', email: 'sonababu@gmail.com'},
+    {id: 4, name: 'hossain', email: 'mjhshikder.dhaka@gmail.com'},
+    {id: 5, name: 'shikder', email: 'mjhshikder.dhaka@gmail.com'},
+]
+
+app.get('/user', (req, res) => {
+    res.send(users)
+})
+
+app.listen(port, () => {
+    console.log(`Users Server Started on port:${port}`);
+    
+})
