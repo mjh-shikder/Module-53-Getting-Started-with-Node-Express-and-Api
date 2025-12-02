a0576786900be6e942f53a542714644ff8ce2d33
mjh-shikder <mjhsikder@gmail.com>
2025-12-02 15:27:45 +0600

    mongodb added and env file created

--- user-management-server/index.js
+++ user-management-server/index.js
@@ -1,15 +1,31 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
+require('dotenv').config;
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const port = process.env.PORT || 4000;
 
 app.get('/', (req, res) => {
     res.send('user server is available');
 })
 
-
+// middleware
 app.use(cors());
 app.use(express.json());
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.tsgn0pw.mongodb.net/?appName=Cluster0`
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+
 
 const users = [
     { id: 1, name: 'jubair', email: 'mjhshikder.dhaka@gmail.com' },
@@ -30,6 +46,20 @@ app.post('/user', (req, res) => {
     users.push(newUser)
     res.send(newUser)
 })
+
+async function run() {
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        
+    }
+}
+run().catch(console.dir);
 
 app.listen(port, () => {
     console.log(`Users Server Started on port:${port}`);
